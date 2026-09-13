598197fbf9466892b148cf209601f99910cc11c3
Pierre <piercel.coder2022@gmail.com>
2026-09-13 17:18:47 +0200

    feat: add organization model

---
+++ app/models/organization.rb
@@ -0,0 +1,14 @@
+class Organization < ApplicationRecord
+  has_many :users, dependent: :destroy
+  has_many :stores, dependent: :destroy
+
+  validates :name, presence: true
+
+  validates :slug,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\A[a-z0-9]+(?:-[a-z0-9]+)*\z/ }
+
+  validates :status,
+    inclusion: { in: %w[active suspended] }
+end
