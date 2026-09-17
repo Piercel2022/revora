febbcab94758b04ad7c5d6638a4bef044522f484
Pierre <piercel.coder2022@gmail.com>
2026-09-18 00:23:18 +0200

    feat: add orders API controller

---
+++ app/controllers/api/v1/orders_controller.rb
@@ -0,0 +1,78 @@
+module Api
+  module V1
+    class OrdersController < ApplicationController
+      before_action :authenticate_user!
+      before_action :set_order, only: %i[show update destroy]
+
+      def index
+        orders = policy_scope(Order)
+
+        render json: orders
+      end
+
+      def show
+        authorize @order
+
+        render json: @order
+      end
+
+      def create
+        store = Store.find(order_params[:store_id])
+        order = store.orders.new(order_params.except(:store_id))
+
+        authorize order
+
+        if order.save
+          render json: order, status: :created
+        else
+          render json: {
+            errors: order.errors.full_messages
+          }, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        authorize @order
+
+        if @order.update(order_params.except(:store_id))
+          render json: @order
+        else
+          render json: {
+            errors: @order.errors.full_messages
+          }, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        authorize @order
+
+        @order.destroy
+
+        head :no_content
+      end
+
+      private
+
+      def set_order
+        @order = Order.find(params[:id])
+      end
+
+      def order_params
+        params.require(:order).permit(
+          :store_id,
+          :customer_id,
+          :external_id,
+          :order_number,
+          :status,
+          :currency,
+          :subtotal,
+          :tax,
+          :shipping,
+          :discount,
+          :total,
+          :ordered_at
+        )
+      end
+    end
+  end
+end
