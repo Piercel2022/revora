2bc9b2e30f3f1fc91902be5b3a3265b717cdab2a
Pierre <piercel.coder2022@gmail.com>
2026-09-17 23:18:13 +0200

    feat: add product authorization policy

---
+++ app/policies/product_policy.rb
@@ -0,0 +1,39 @@
+class ProductPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    same_organization?
+  end
+
+  def create?
+    same_organization? && management_role?
+  end
+
+  def update?
+    same_organization? && management_role?
+  end
+
+  def destroy?
+    same_organization? && user.owner?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    def resolve
+      scope.joins(:store).where(
+        stores: { organization_id: user.organization_id }
+      )
+    end
+  end
+
+  private
+
+  def same_organization?
+    record.store.organization_id == user.organization_id
+  end
+
+  def management_role?
+    user.owner? || user.admin?
+  end
+end
