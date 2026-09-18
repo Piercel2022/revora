99b9aa6f46dab307c8a1d4faf9082af8b6f82c44
Pierre <piercel.coder2022@gmail.com>
2026-09-18 15:33:51 +0200

    feat: associate orders with order items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :store
   belongs_to :customer, optional: true
+  has_many :order_items, dependent: :destroy
 
   validates :external_id,
     presence: true,
